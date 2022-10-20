62b7f7a7b8639f03e9423121553c6443bd2c2883
danny-englander <dannyenglander311@gmail.com>
2022-10-20 08:06:35 -0400

    Adding API call

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -5,6 +5,30 @@ import Pagination from "./Pagination";
 import ArtCard from "./ArtCard";
 
 const Art = () => {
+  // Set the artworks as an emoty array with useState.
+  const [artWorks, getArtWorks] = useState([]);
+  // Set the search term to an empty string.
+  const [searchTerm, setSearchTerm] = useState("");
+  // Set the page numbers to start at 0.
+  const [pageNumber, setPageNumber] = useState(0);
+  // Set the number of artworks per page.
+  const artworksPerPage = 12;
+  const pagesVisited = pageNumber * artworksPerPage;
+
+  useEffect(() => {
+    async function fetchArtWorks() {
+      const URL = `https://api.artic.edu/api/v1/artworks?limit=20`;
+      try {
+        const response = await axios.get(URL);
+        console.log(response.data);
+        getArtWorks(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchArtWorks();
+  }, []);
+
   return (
     <>
       hello
